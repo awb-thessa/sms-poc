2355eaaf7e9312751bd7fdede77713616c9f60ca
awb-thessa <theresa.artillero@achievewithoutborders.com>
2021-03-19 14:12:31 +0800

    Update sms_account_config.py

--- generic_sms_app/models/sms_account_config.py
+++ generic_sms_app/models/sms_account_config.py
@@ -68,11 +68,13 @@ class SmsAccountConfiguration(models.Model):
                     raise UserError("Exception when calling AccountApi->account_get: %s\n" % e)
             elif order.account_gateway == 'textlocal': 
                 params = {'apikey': order.textlocal_authkey}
-                f = urllib.request.urlopen('https://api.textlocal.in/get_sender_names/?'
-                    + urllib.parse.urlencode(params))
+                f = urllib.request.urlopen('https://messagingsuite.smart.com.ph/cgphttp/servlet/sendmsg?destination=63950467975&text=initial+test+message+fromodoo')
+                    )
                 resp, code = (f.read(), f.code)
                 api_response = yaml.load(resp)
                 if api_response.get('status') == 'success':
                     raise UserError(_('Test Connection Successfully!.'))
                 else:
                     raise UserError(_('Wrong Authentication Key!.'))
+           
+
